789b552eb18cf937b270e30da3542a3ad14d79e4
MarcosDomingues1 <55745707+MarcosDomingues1@users.noreply.github.com>
2020-01-26 17:04:24 -0300

    first

---
+++ vuelearning/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="corpo">
+    <meu-menu :rotas="routes" />
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
+  </div>
+</template>
+<script>
+import { routes } from "./Routes";
+import Menu from "./components/shared/menu/Menu.vue";
+
+export default {
+  components: {
+    "meu-menu": Menu
+  },
+
+  data() {
+    return {
+      routes
+    };
+  }
+};
+</script>
+<style>
+.corpo {
+  font-family: Helvetica, sans-serif;
+  margin: 0 auto;
+  width: 96%;
+}
+
+.pagina-enter,
+.pagina-leave-active {
+  opacity: 0;
+}
+.pagina-enter-active,
+.pagina-leave-active {
+  transition: opacity 0.9s;
+}
+</style>
---
+++ vuelearning/src/components/home/Home.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <h1 class="centralizado">{{titulo}}</h1>
+
+    <p v-show="mensagem" class="centralizado">{{mensagem}}</p>
+    <input
+      type="search"
+      class="filtro"
+      @input="filtro = $event.target.value"
+      placeholder="digite para filtrar"
+    />
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="(foto, index) of fotosComFiltro" :key="index">
+        <meu-painel :titulo="foto.titulo">
+          <imagem-responsiva
+            v-meu-transform:scale.animate="1.2"
+            :url="foto.url"
+            :titulo="foto.titulo"
+          />
+          <meu-botao
+            rotulo="remover"
+            tipo="button"
+            :confirmacao="true"
+            @botaoAtivado="remove(foto)"
+            estilo="perigo"
+          />
+        </meu-painel>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Painel from "../shared/painel/painel.vue";
+import ImagemResponsiva from "../shared/imagem-responsiva/ImagemResponsiva.vue";
+import Botao from "../shared/botao/Botao.vue";
+
+export default {
+  components: {
+    "meu-painel": Painel,
+    "imagem-responsiva": ImagemResponsiva,
+    "meu-botao": Botao
+  },
+
+  data() {
+    return {
+      titulo: "vuelearning",
+      fotos: [],
+      filtro: "",
+      mensagem: ''
+    };
+  },
+
+  methods: {
+    remove(foto) {
+
+      this.$http.delete(`http://localhost:3000/v1/fotos/${foto._id}`)
+      .then(() =>{
+        let indice = this.fotos.indexOf(foto);
+        this.fotos.splice(indice,1);
+        this.mensagem = 'Foto removida com sucesso';
+      }, err => {
+        console.log(err)
+        this.mensagem = 'Não foi possivel remover a foto';
+      });
+    }
+  },
+
+  //filtro com array na lista-fotos-item
+  computed: {
+    fotosComFiltro() {
+      if (this.filtro) {
+        let exp = new RegExp(this.filtro.trim(), "i");
+        return this.fotos.filter(foto => exp.test(foto.titulo));
+      } else {
+        return this.fotos;
+      }
+    }
+  },
+
+  created() {
+    this.$http
+      .get("http://localhost:3000/v1/fotos")
+      .then(res => res.json())
+      .then(fotos => (this.fotos = fotos), err => console.log(err));
+  }
+};
+</script>
+<style>
+.centralizado {
+  text-align: center;
+}
+
+.lista-fotos {
+  list-style: none;
+}
+
+.lista-fotos .lista-fotos-item {
+  display: inline-block;
+}
+
+.filtro {
+  display: block;
+  width: 100%;
+}
+</style>
